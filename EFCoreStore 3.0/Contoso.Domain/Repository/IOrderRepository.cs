fb4c72a27d0fe0f549fd45b043c92d729e64633f
zheng ming <zheng1748@outlook.com>
2019-11-03 15:00:22 +0800

    IRepository

--- EFCoreStore 3.0/Contoso.Domain/Repository/IOrderRepository.cs
+++ EFCoreStore 3.0/Contoso.Domain/Repository/IOrderRepository.cs
@@ -9,10 +9,34 @@ using Contoso.Domain.Models;
 
 namespace Contoso.Domain.Repository
 {
-    public interface IOrderRepository 
+    public interface IOrderRepository
     {
-       ValueTask<IEnumerable<Order>> InsertOrdersAsync(IEnumerable<Order> orders);
+        #region Owned
+        ValueTask<IEnumerable<SaleOrder>> InsertSaleOrdersAsync(IEnumerable<SaleOrder> saleOrders);
+
+        ValueTask<IPagedList<SaleOrder>> GetPagedSaleOrdersAsync(int pageIndex = 0, int pageSize = int.MaxValue);
+
+        ValueTask<IEnumerable<Distributor>> InsertDistributorsAsync(IEnumerable<Distributor> distributors);
+
+        ValueTask<IPagedList<Distributor>> GetPagedDistributorsAsync(int pageIndex = 0, int pageSize = int.MaxValue);
+        #endregion
+
+        #region 1  to  0..1
+        ValueTask<IEnumerable<Instructor>> InsertInstructorsAsync(IEnumerable<Instructor> instructors);
+
+        ValueTask<IPagedList<Instructor>> GetPagedInstructorsAsync(int pageIndex = 0, int pageSize = int.MaxValue);
+        #endregion
+
+        #region TPH
+        ValueTask<IEnumerable<Employee>> InsertEmployeesAsync(IEnumerable<Employee> employees);
+
+        ValueTask<IPagedList<Employee>> GetPagedEmployeesAsync(int pageIndex = 0, int pageSize = int.MaxValue);
+        #endregion
+
+        #region Order 1 to ..
+        ValueTask<IEnumerable<Order>> InsertOrdersAsync(IEnumerable<Order> orders);
 
         ValueTask<IPagedList<Order>> GetPagedOrdersAsync(int pageIndex = 0, int pageSize = int.MaxValue);
+        #endregion
     }
 }
